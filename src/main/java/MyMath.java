f325b8a6885882ad1cf41ff13a817d66f3ab1d73
Гукасян Григорий <73351839+griguk77@users.noreply.github.com>
2022-10-07 15:37:37 +0300

    Update MyMath.java

--- src/main/java/MyMath.java
+++ src/main/java/MyMath.java
@@ -1,13 +1,1 @@
-public class MyMath {
-    public double divide(int numberFirst, int numberSecond) {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        if (numberSecond == 0)
-            throw new RuntimeException("Can't divide by 0");
-
-        return numberFirst / numberSecond;
-    }
-}
+Я гей
